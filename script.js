e995c5a06e530767f493648f26ef1e2aeadbe580
Svetlana <svetlana_el12@mail.ru>
2022-04-06 02:12:36 +0500

    new block: description

--- script.js
+++ script.js
@@ -5,47 +5,46 @@ function burgerMenu() {
     let spanBurgerBottom = document.querySelector('.menu__button_before');
 
     let burgerMenuNav = document.querySelector('.nav-mobile');
-    
+
+    function closeMenu() {
+        //меняем крестик на бургер
+        spanBurgerMiddle.style.display = 'block';
+        spanBurgerTop.style.transform = '';
+        spanBurgerTop.style.top = '0';
+        spanBurgerBottom.style.transform = '';
+        spanBurgerBottom.style.bottom = '0';
+
+        //скрываем меню
+        burgerMenuNav.style.animation = '1s closed';
+        burgerMenuNav.style.right = '-250px';
+    }
+
+    function showMenu() {
+        spanBurgerMiddle.style.display = 'none';
+        spanBurgerTop.style.transform = 'rotate(45deg)';
+        spanBurgerTop.style.top = '40%';
+        spanBurgerBottom.style.transform = 'rotate(135deg)';
+        spanBurgerBottom.style.bottom = '40%';
+
+
+        burgerMenuNav.style.animation = '1s show';
+        burgerMenuNav.style.right = '0';
+    }
 
     burgerBtn.addEventListener('click', function () {
         if (spanBurgerMiddle.style.display === 'none') {
-            //меняем крестик на бургер
-            spanBurgerMiddle.style.display = 'block';
-            spanBurgerTop.style.transform = '';
-            spanBurgerTop.style.top = '0';
-            spanBurgerBottom.style.transform = '';
-            spanBurgerBottom.style.bottom = '0';
+            closeMenu();
 
-            //скрываем меню
-            burgerMenuNav.style.animation = '1s closed';
-            burgerMenuNav.style.right = '-250px';
-            
         } else {
-            spanBurgerMiddle.style.display = 'none';
-            spanBurgerTop.style.transform = 'rotate(45deg)';
-            spanBurgerTop.style.top = '40%';
-            spanBurgerBottom.style.transform = 'rotate(135deg)';
-            spanBurgerBottom.style.bottom = '40%';
-
-            
-            burgerMenuNav.style.animation = '1s show';
-            burgerMenuNav.style.right = '0';
-            
+            showMenu();
         }
-
     })
 
+    //клик вне поля закроет меню
     document.addEventListener('click', function (e) {
-        if (e.target !== burgerBtn && e.target !== spanBurgerTop && e.target !== spanBurgerMiddle && e.target !== spanBurgerBottom && e.target !== burgerMenuNav) {
-            spanBurgerMiddle.style.display = 'block';
-            spanBurgerTop.style.transform = '';
-            spanBurgerTop.style.top = '0';
-            spanBurgerBottom.style.transform = '';
-            spanBurgerBottom.style.bottom = '0';
-
-            burgerMenuNav.style.animation = '1s closed';
-            burgerMenuNav.style.right = '-250px';
-        }   
+        if (e.target !== burgerBtn && e.target !== spanBurgerTop && e.target !== spanBurgerMiddle && e.target !== spanBurgerBottom && e.target !== burgerMenuNav && (spanBurgerMiddle.style.display === 'none')) {
+            closeMenu();
+        }
     })
 }
 
