6c0e777f84172993e6dca77e04786111355b4c76
Svetlana <svetlana_el12@mail.ru>
2022-04-05 21:48:53 +0500

    new block: overview

---
+++ script.js
@@ -0,0 +1,52 @@
+function burgerMenu() {
+    let burgerBtn = document.querySelector('.menu-burger');
+    let spanBurgerTop = document.querySelector('.menu__button_after');
+    let spanBurgerMiddle = document.querySelector('.menu__button-label');
+    let spanBurgerBottom = document.querySelector('.menu__button_before');
+
+    let burgerMenuNav = document.querySelector('.nav-mobile');
+    
+
+    burgerBtn.addEventListener('click', function () {
+        if (spanBurgerMiddle.style.display === 'none') {
+            //меняем крестик на бургер
+            spanBurgerMiddle.style.display = 'block';
+            spanBurgerTop.style.transform = '';
+            spanBurgerTop.style.top = '0';
+            spanBurgerBottom.style.transform = '';
+            spanBurgerBottom.style.bottom = '0';
+
+            //скрываем меню
+            burgerMenuNav.style.animation = '1s closed';
+            burgerMenuNav.style.right = '-250px';
+            
+        } else {
+            spanBurgerMiddle.style.display = 'none';
+            spanBurgerTop.style.transform = 'rotate(45deg)';
+            spanBurgerTop.style.top = '40%';
+            spanBurgerBottom.style.transform = 'rotate(135deg)';
+            spanBurgerBottom.style.bottom = '40%';
+
+            
+            burgerMenuNav.style.animation = '1s show';
+            burgerMenuNav.style.right = '0';
+            
+        }
+
+    })
+
+    document.addEventListener('click', function (e) {
+        if (e.target !== burgerBtn && e.target !== spanBurgerTop && e.target !== spanBurgerMiddle && e.target !== spanBurgerBottom && e.target !== burgerMenuNav) {
+            spanBurgerMiddle.style.display = 'block';
+            spanBurgerTop.style.transform = '';
+            spanBurgerTop.style.top = '0';
+            spanBurgerBottom.style.transform = '';
+            spanBurgerBottom.style.bottom = '0';
+
+            burgerMenuNav.style.animation = '1s closed';
+            burgerMenuNav.style.right = '-250px';
+        }   
+    })
+}
+
+burgerMenu();
